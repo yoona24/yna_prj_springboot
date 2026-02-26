766a5e3e4f18da30b9bf7346674c958b4191c964
yoona24 <estelle0204@naver.com>
2026-02-27 00:27:03 +0900

    관리자 시작

--- backend-spring/src/main/java/com/yaprj/controller/AdminAuthController.java
+++ backend-spring/src/main/java/com/yaprj/controller/AdminAuthController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/admin/auth")
 @RequiredArgsConstructor
-public class AdminAuthController {
+public class AdminAuthController { //관리자 기능
     
     private final AdminService adminService;
     
